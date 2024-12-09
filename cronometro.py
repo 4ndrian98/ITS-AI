54d84478242c3239db1864c85a65ff4571e81629
Andrian Ghiba <andrian.ghiba98@gmail.com>
2024-12-09 11:09:07 +0100

    import file esercizio cronometro python

---
+++ cronometro.py
@@ -0,0 +1,11 @@
+import time 
+tempo = int(input("Scrivi in secondi il tempo: "))
+
+for x in range(tempo, 0, -1):
+    secondi= x%60
+    minuti= int(x/60)%60
+    ore=int(x/3600)
+    time.sleep(1)
+    print(f"{ore:02}:{minuti:02}:{secondi:02}")
+
+print("Tempo Finito!")
