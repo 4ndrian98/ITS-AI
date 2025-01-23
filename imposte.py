c3147e1e7fdd88164106783e51998b435c7f1d6f
Andrian Ghiba <andrian.ghiba98@gmail.com>
2025-01-23 12:53:44 +0100

    imposte.py

--- imposte.py
+++ imposte.py
@@ -6,19 +6,17 @@ reddito = float(input("Inserisci il tuo reddito: "))
 tasse_da_scontare = 0.0
 reddito_netto = 0.0
 
-# Calcolo prima aliquota
 if reddito > 28000:
     tasse_da_scontare += 28000 * PRIMA_ALIQUOTA
 else:
     tasse_da_scontare += reddito * PRIMA_ALIQUOTA
 
-# Calcolo seconda aliquota
-if 28000 < reddito <= 50000:
-    tasse_da_scontare += (reddito - 28000) * SECONDA_ALIQUOTA
-elif reddito > 50000:
-    tasse_da_scontare += 22000 * SECONDA_ALIQUOTA 
+if reddito > 28000:
+    if reddito <= 50000:
+        tasse_da_scontare += (reddito - 28000) * SECONDA_ALIQUOTA
+    else:
+        tasse_da_scontare += (50000 - 28000) * SECONDA_ALIQUOTA
 
-# Calcolo terza aliquota
 if reddito > 50000:
     tasse_da_scontare += (reddito - 50000) * TERZA_ALIQUOTA
 
@@ -26,7 +24,6 @@ print("Le tasse da scontare sono:", tasse_da_scontare, "€")
 
 reddito_netto = reddito - tasse_da_scontare
 print("Il tuo reddito al netto delle tasse è:", reddito_netto, "€")
-    
     
 
 
